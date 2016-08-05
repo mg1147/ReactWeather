6b14f9daef12e288293acd077454a2510a7ab211
Gerd Deibel <gerddeibel@Gerds-MBP-2.fritz.box>
2016-08-05 19:26:44 +0200

    Style  About

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -1,10 +1,12 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About  = (props) => {
   return (
     <div>
-    <h3>About page</h3>
-    <p>Hallo hier ist text</p>
+    <h1 className="text-center">Über</h1>
+    <p>Das ist eine Wetter App fürs Web. Absolut ohne sinn und zweck.</p>
+    <Link to='http://react.de'>Link zu React</Link>
     </div>
   )
 };
